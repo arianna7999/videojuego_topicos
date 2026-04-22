ca1f82d276f6bed72dc596123331949de4f1e0e6
Eduardo <eduardorochin19@outlook.com>
2026-04-21 18:55:52 -0700

    agrega parte de las funciones para elegir el personaje

---
+++ src/elementos/Personaje.java
@@ -0,0 +1,42 @@
+package elementos;
+
+public class Personaje {
+
+    public String nombre;
+    public int vida;
+    public float velocidad;
+    public int daño;
+    public float salto;
+    public String sprite;
+    public int spriteColumnas;
+    public int spriteFilas;
+    public int spriteCeldaW;
+    public int spriteCeldaH;
+    public String habilidad;
+    public float drawOffsetX;
+    public float drawOffsetY;
+    public int tiempoParaCurar;
+    public int cantidadCuraAutomatica;
+
+    public Personaje(String nombre, int vida, float velocidad, int daño, float salto,
+                     String sprite, int spriteColumnas, int spriteFilas,
+                     int spriteCeldaW, int spriteCeldaH, String habilidad,
+                     float drawOffsetX, float drawOffsetY,
+                     int tiempoParaCurar, int cantidadCuraAutomatica) {
+        this.nombre = nombre;
+        this.vida = vida;
+        this.velocidad = velocidad;
+        this.daño = daño;
+        this.salto = salto;
+        this.sprite = sprite;
+        this.spriteColumnas = spriteColumnas;
+        this.spriteFilas = spriteFilas;
+        this.spriteCeldaW = spriteCeldaW;
+        this.spriteCeldaH = spriteCeldaH;
+        this.habilidad = habilidad;
+        this.drawOffsetX = drawOffsetX;
+        this.drawOffsetY = drawOffsetY;
+        this.tiempoParaCurar = tiempoParaCurar;
+        this.cantidadCuraAutomatica = cantidadCuraAutomatica;
+    }
+}
